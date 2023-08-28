cc5bf201a1ecdb0b750270b476ba3c2ee432634a
selahaddinFurkanYildirim <110099952+selahaddinFurkanYildirim@users.noreply.github.com>
2023-08-28 10:35:30 +0300

    Delete www directory

--- www/php/sendMessage.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file=fopen("../data/mesajlar.txt","a");
-fwrite($file,($_POST['mesaj']."\n"));
-fclose($file);
-echo "$name<script>window.location = '/index.html'</script>";
-?>
--- www/sounds/a.sh
+++
@@ -1,4 +0,0 @@
-while [ 1 -gt 0 ]; do
-    google-chrome ses.m4a;
-    sleep 900;
-done
